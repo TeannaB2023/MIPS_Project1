9f056d629f83570d422be9655a9c74c6d2bb6233
TeannaB2023 <teanna.barrett@bison.howard.edu>
2020-10-12 19:51:39 -0500

    Fixed program to take 10 characters

--- project1.s
+++ project1.s
@@ -1,13 +1,13 @@
 # MIPS Programming Project 1
 	.data # This is the section to declare variables that will be used in the program
-	x: .space 9 # The space for the id is initialized (an id only has 8 characters)
+	x: .space 11 # The space for the id is initialized (an id only has 10 characters)
 
 	.text # This is the section where the instructions will be written
 main:
 	li	$v0, 8	# load value of 8 so syscall knows that it will be reading from and story the input
 	la	$a0, x	# Loads the address of the id input to $a0 register
-	li	$a1, 9	# Loads the amount of space that is allocated for the input 
-	syscall	# Completes the read string instruction
+	li	$a1, 11	# Loads the amount of space that is allocated for the input 
+	syscall		# Completes the read string instruction
 	
 	add	$t0, $zero, $zero	# Temporarily hold a test value that will eventually represent sum
 	add	$t3, $zero, $zero	# Keeps track of the increments for the loop
@@ -24,12 +24,12 @@ LOWER:
 NOT:					# If not the sum is skipped over
 	addi	$a0, $a0, 1		# Increment the address by one
 	addi	$t3, $t3, 1		# Increment the loop counter by 1	
-	slti	$t4, $t3, 8		# This will check if current place of the loop is still on the input 
-	bne	$t4, $zero, START	# The program will loop for only 8 iterations
-
-	sw	$t0, 0($a0)	# Store the sum in memory right after the id string
+	slti	$t4, $t3, 10		# This will check if current place of the loop is still on the input 
+	bne	$t4, $zero, START	# The program will loop for only 10 iterations
+			
+	sh	$t0, 0($a0)	# Store the sum in memory right after the id string
 	li	$v0, 1		# Loads value that tells syscall to print
-	lw	$a0, 0($a0)	# Load the sum from memory so it can be printed
+	lh	$a0, 0($a0)	# Load the sum from memory so it can be printed
 	syscall			# Completes the print instruction
 
 	li	$v0, 10		# Exit program call
